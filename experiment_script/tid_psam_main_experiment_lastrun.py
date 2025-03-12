b6533914e47d5b328ff9b084165cef82ae39a096
Tim Dreßler <tim.dressler@uni-oldenburg.de>
2025-03-12 16:09:45 +0100

    Minor Updates

--- experiment_script/tid_psam_main_experiment_lastrun.py
+++ experiment_script/tid_psam_main_experiment_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on März 10, 2025, at 15:01
+    on März 12, 2025, at 15:16
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
