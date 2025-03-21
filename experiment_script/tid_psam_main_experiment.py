be3240d5bc484c106b087eb49a4144cae15d775f
timdressler <tim.dressler@uni-oldenburg.de>
2025-03-21 15:43:02 +0100

    Minor Update Experiment LAB3

--- experiment_script/tid_psam_main_experiment.py
+++ experiment_script/tid_psam_main_experiment.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on März 20, 2025, at 13:27
+    on März 21, 2025, at 15:38
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -425,6 +425,7 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         lineWidth=1.0,
         colorSpace='rgb', lineColor='white', fillColor='white',
         opacity=None, depth=0.0, interpolate=True)
+    fixation_cross_marker = parallel.ParallelPort(address='0x3ff8')
     
     # --- Initialize components for Routine "trial" ---
     cue_stim = visual.ShapeStim(
@@ -476,7 +477,6 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         recordingFolder=micRecFolder,
         recordingExt='wav'
     )
-    probe_marker = parallel.ParallelPort(address='0x3ff8')
     
     # create some handy timers
     
@@ -803,7 +803,7 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         # create an object to store info about Routine ISI
         ISI = data.Routine(
             name='ISI',
-            components=[fixation_cross_ISI],
+            components=[fixation_cross_ISI, fixation_cross_marker],
         )
         ISI.status = NOT_STARTED
         continueRoutine = True
@@ -864,6 +864,35 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
             if fixation_cross_ISI.status == STARTED:
                 # update params
                 pass
+            # *fixation_cross_marker* updates
+            
+            # if fixation_cross_marker is starting this frame...
+            if fixation_cross_marker.status == NOT_STARTED and t >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                fixation_cross_marker.frameNStart = frameN  # exact frame index
+                fixation_cross_marker.tStart = t  # local t and not account for scr refresh
+                fixation_cross_marker.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(fixation_cross_marker, 'tStartRefresh')  # time at next scr refresh
+                # add timestamp to datafile
+                thisExp.addData('fixation_cross_marker.started', t)
+                # update status
+                fixation_cross_marker.status = STARTED
+                fixation_cross_marker.status = STARTED
+                win.callOnFlip(fixation_cross_marker.setData, int(1))
+            
+            # if fixation_cross_marker is stopping this frame...
+            if fixation_cross_marker.status == STARTED:
+                # is it time to stop? (based on global clock, using actual start)
+                if tThisFlipGlobal > fixation_cross_marker.tStartRefresh + 1.0-frameTolerance:
+                    # keep track of stop time/frame for later
+                    fixation_cross_marker.tStop = t  # not accounting for scr refresh
+                    fixation_cross_marker.tStopRefresh = tThisFlipGlobal  # on global time
+                    fixation_cross_marker.frameNStop = frameN  # exact frame index
+                    # add timestamp to datafile
+                    thisExp.addData('fixation_cross_marker.stopped', t)
+                    # update status
+                    fixation_cross_marker.status = FINISHED
+                    win.callOnFlip(fixation_cross_marker.setData, int(0))
             
             # check for quit (typically the Esc key)
             if defaultKeyboard.getKeys(keyList=["escape"]):
@@ -904,6 +933,8 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         ISI.tStop = globalClock.getTime(format='float')
         ISI.tStopRefresh = tThisFlipGlobal
         thisExp.addData('ISI.stopped', ISI.tStop)
+        if fixation_cross_marker.status == STARTED:
+            win.callOnFlip(fixation_cross_marker.setData, int(0))
         # the Routine "ISI" was not non-slip safe, so reset the non-slip timer
         routineTimer.reset()
         
@@ -911,7 +942,7 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         # create an object to store info about Routine trial
         trial = data.Routine(
             name='trial',
-            components=[cue_stim, target_stim, probe_stim, task_msg, mic, probe_marker],
+            components=[cue_stim, target_stim, probe_stim, task_msg, mic],
         )
         trial.status = NOT_STARTED
         continueRoutine = True
@@ -1176,35 +1207,6 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
                     mic.status = FINISHED
                     # stop recording with mic
                     mic.stop()
-            # *probe_marker* updates
-            
-            # if probe_marker is starting this frame...
-            if probe_marker.status == NOT_STARTED and t >= probe_onset-frameTolerance:
-                # keep track of start time/frame for later
-                probe_marker.frameNStart = frameN  # exact frame index
-                probe_marker.tStart = t  # local t and not account for scr refresh
-                probe_marker.tStartRefresh = tThisFlipGlobal  # on global time
-                win.timeOnFlip(probe_marker, 'tStartRefresh')  # time at next scr refresh
-                # add timestamp to datafile
-                thisExp.addData('probe_marker.started', t)
-                # update status
-                probe_marker.status = STARTED
-                probe_marker.status = STARTED
-                win.callOnFlip(probe_marker.setData, int(1))
-            
-            # if probe_marker is stopping this frame...
-            if probe_marker.status == STARTED:
-                # is it time to stop? (based on global clock, using actual start)
-                if tThisFlipGlobal > probe_marker.tStartRefresh + probe_duration-frameTolerance:
-                    # keep track of stop time/frame for later
-                    probe_marker.tStop = t  # not accounting for scr refresh
-                    probe_marker.tStopRefresh = tThisFlipGlobal  # on global time
-                    probe_marker.frameNStop = frameN  # exact frame index
-                    # add timestamp to datafile
-                    thisExp.addData('probe_marker.stopped', t)
-                    # update status
-                    probe_marker.status = FINISHED
-                    win.callOnFlip(probe_marker.setData, int(0))
             
             # check for quit (typically the Esc key)
             if defaultKeyboard.getKeys(keyList=["escape"]):
@@ -1257,8 +1259,6 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         trials.addData(
             'mic.clip', mic.recordingFolder / mic.getClipFilename(tag)
         )
-        if probe_marker.status == STARTED:
-            win.callOnFlip(probe_marker.setData, int(0))
         # using non-slip timing so subtract the expected duration of this Routine (unless ended on request)
         if trial.maxDurationReached:
             routineTimer.addTime(-trial.maxDuration)
