e1c396206a9d76626820ce7029109142814831fe
timdressler <tim.dressler@uni-oldenburg.de>
2025-04-09 16:55:06 +0200

    Minor Changes LAB3

--- experiment_script/tid_psam_main_experiment_ALTERNATIVE_NO_CIRCLE.py
+++ experiment_script/tid_psam_main_experiment_ALTERNATIVE_NO_CIRCLE.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on April 08, 2025, at 08:36
+    on April 08, 2025, at 09:58
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
