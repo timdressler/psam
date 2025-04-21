bca40d4f82d0ca2de18eff7d25caa733158da805
Tim Dreßler <tim.dressler@uni-oldenburg.de>
2025-04-21 09:10:36 +0200

    Major Updates

--- analysis_script/MATLAB/tid_psam_questionnaire_input.m
+++ analysis_script/MATLAB/tid_psam_questionnaire_input.m
@@ -27,10 +27,6 @@ addpath(FUNPATH);
 
 tid_psam_check_folder_TD(MAINPATH, INPATH, OUTPATH)
 
-% Initialize sanity check variables
-protocol = {};
-
-% FAL
 % Get filename
 fal_filename = fullfile(INPATH, 'tid_psam_fal_data.xlsx');
 
@@ -102,92 +98,230 @@ subj = ['sub-' formattedNum]; % Construct final subject ID
 
 % Check if the subject already exists in the table
 if any(strcmp(fal.subj, subj))
-    choice = questdlg([subj, ' already exists. Do you want to overwrite the existing data?'], ...
+    choice = questdlg([subj, ' already exists in FAL. Do you want to overwrite the existing data?'], ...
         'Duplicate Subject', 'Overwrite', 'Cancel', 'Cancel');
     if strcmp(choice, 'Cancel')
         disp('Operation cancelled.');
-        return;
+        fal_skipped = 1;
     elseif strcmp(choice, 'Overwrite')
         disp(['Existing data for ', subj, ' will be overwritten.']);
-        % Find the row and overwrite it 
+        % Find the row and overwrite it
         rowIdx = find(strcmp(fal.subj, subj));
-        fal(rowIdx, :) = [];  
+        fal(rowIdx, :) = [];
+        fal_skipped = 0;
     end
+else
+    fal_skipped = 0;
 end
 
-% Create nwe row
-newRow = cell(1, width(fal));
-newRow{1} = subj;
+if fal_skipped ~= 1
+    % Create new row
+    newRow = cell(1, width(fal));
+    newRow{1} = subj;
 
-% Input values for each variables
-for i = 2:width(fal)
-    varName = fal.Properties.VariableNames{i};
-    if ~endsWith(varName, '_detail') % Only a
-        while true
-            answer = inputdlg(fal_promptTexts(varName), 'Data Entry', [1 70]);
-            if isempty(answer)
-                disp('Cancelled.');
-                return;
+    % Input values for each variables
+    for i = 2:width(fal)
+        varName = fal.Properties.VariableNames{i};
+        if ~endsWith(varName, '_detail') % Only a
+            while true
+                answer = inputdlg(fal_promptTexts(varName), 'FAL', [1 70]);
+                if isempty(answer)
+                    disp('Cancelled.');
+                    return;
+                end
+                % If the answer is empty, keep prompting
+                if isempty(answer{1})
+                    disp('This field cannot be left empty. Please enter a valid response.');
+                else
+                    % Otherwise, store the answer and break out of the loop
+                    newRow{i} = answer{1};
+                    break;
+                end
             end
-            % If the answer is empty, keep prompting
-            if isempty(answer{1})
-                disp('This field cannot be left empty. Please enter a valid response.');
-            else
-                % Otherwise, store the answer and break out of the loop
-                newRow{i} = answer{1};
-                break;
+        elseif strcmp(newRow{i-1},'yes') % Only ask for detail if needed
+            while true
+                answer = inputdlg(fal_promptTexts(varName), 'FAL', [1 70]);
+                if isempty(answer)
+                    disp('Cancelled.');
+                    return;
+                end
+                % If the answer is empty, keep prompting
+                if isempty(answer{1})
+                    disp('This field cannot be left empty. Please enter a valid response.');
+                else
+                    % Otherwise, store the answer and break out of the loop
+                    newRow{i} = answer{1};
+                    break;
+                end
             end
+        else
+            newRow{i} = 'NaN';
         end
-    elseif strcmp(newRow{i-1},'yes') % Only ask for detail if needed
-        while true
-            answer = inputdlg(fal_promptTexts(varName), 'Data Entry', [1 70]);
-            if isempty(answer)
-                disp('Cancelled.');
-                return;
-            end
-            % If the answer is empty, keep prompting
-            if isempty(answer{1})
-                disp('This field cannot be left empty. Please enter a valid response.');
-            else
-                % Otherwise, store the answer and break out of the loop
-                newRow{i} = answer{1};
-                break;
-            end
+    end
+
+    % Add new row to the table
+    newRowTable = cell2table(newRow, 'VariableNames', fal.Properties.VariableNames);
+    for i = 1:width(fal)
+        if iscell(fal{:, i}) && ~iscell(newRowTable{:, i})
+            newRowTable{:, i} = {newRowTable{:, i}};
         end
-    else
-        newRow{i} = 'NaN';
     end
+    fal = [fal; newRowTable];
 end
-
-% Add new row to the table
-newRowTable = cell2table(newRow, 'VariableNames', fal.Properties.VariableNames);
-for i = 1:width(fal)
-    if iscell(fal{:, i}) && ~iscell(newRowTable{:, i})
-        newRowTable{:, i} = {newRowTable{:, i}};
-    end
-end
-fal = [fal; newRowTable];
-
-% Update Protocol
-protocol{subj_idx,1} = 'FAL OK';
 
 % NASA-TLX
+% Get filename
+nasatlx_filename = fullfile(INPATH, 'tid_psam_nasatlx_data.xlsx');
+
+% Load or create FAL data
+if exist(nasatlx_filename, 'file') ~= 2
+    nasatlx_varNames = {
+        'subj', 'var1_mental_demand', 'var2_physical_demand', 'var3_performance', 'var4_effort', ...
+        'var5_frustration'
+        };
+    nasatlx = cell2table(cell(0, numel(nasatlx_varNames)), 'VariableNames', nasatlx_varNames);
+else
+    nasatlx = readtable(nasatlx_filename);
+end
+
+% Check if the subject already exists in the table
+if any(strcmp(nasatlx.subj, subj))
+    choice = questdlg([subj, ' already exists in NASA-TLX. Do you want to overwrite the existing data?'], ...
+        'Duplicate Subject', 'Overwrite', 'Cancel', 'Cancel');
+    if strcmp(choice, 'Cancel')
+        disp('Operation cancelled.');
+        nasatlx_skipped = 1;
+    elseif strcmp(choice, 'Overwrite')
+        disp(['Existing data for ', subj, ' will be overwritten.']);
+        % Find the row and overwrite it
+        rowIdx = find(strcmp(nasatlx.subj, subj));
+        nasatlx(rowIdx, :) = [];
+        nasatlx_skipped = 0;
+    end
+else
+    nasatlx_skipped = 0;
+end
+
+if nasatlx_skipped ~= 1
+    % Create new row
+    newRow = cell(1, width(nasatlx));
+    newRow{1} = subj;
+
+    % Input values for each variables
+    for i = 2:width(nasatlx)
+        varName = nasatlx.Properties.VariableNames{i};
+
+        while true
+            answer = inputdlg([varName ' (Angeben: Ganze Zahl oder Dezimal mit Punkt (nicht Komma))'], 'NASA-TLX', [1 70]);
+            if isempty(answer)
+                disp('Cancelled.');
+                return;
+            end
+            % If the answer is empty, keep prompting
+            if isempty(answer{1})
+                disp('This field cannot be left empty. Please enter a valid response.');
+            else
+                % Otherwise, store the answer and break out of the loop
+                newRow{i} = answer{1};
+                break;
+            end
+        end
+    end
+
+
+    % Add new row to the table
+    newRowTable = cell2table(newRow, 'VariableNames', nasatlx.Properties.VariableNames);
+    for i = 1:width(nasatlx)
+        if iscell(nasatlx{:, i}) && ~iscell(newRowTable{:, i})
+            newRowTable{:, i} = {newRowTable{:, i}};
+        end
+    end
+    nasatlx = [nasatlx; newRowTable];
+end
 
 % SAM
+% Get filename
+sam_filename = fullfile(INPATH, 'tid_psam_sam_data.xlsx');
 
+% Load or create FAL data
+if exist(sam_filename, 'file') ~= 2
+    sam_varNames = {
+        'subj', 'var1_mood_break1', 'var2_tiredness_break1', 'var3_mood_break2', 'var4_tiredness_break2', ...
+        'var5_mood_break3', 'var6_tiredness_break3','var7_mood_break4', 'var8_tiredness_break4','var9_mood_break5', 'var10_tiredness_break5',...
+        'var11_mood_break6', 'var12_tiredness_break6', 'var13_mood_break7', 'var14_tiredness_break7'
+        };
+    sam = cell2table(cell(0, numel(sam_varNames)), 'VariableNames', sam_varNames);
+else
+    sam = readtable(sam_filename);
+end
 
+% Check if the subject already exists in the table
+if any(strcmp(sam.subj, subj))
+    choice = questdlg([subj, ' already exists in SAM. Do you want to overwrite the existing data?'], ...
+        'Duplicate Subject', 'Overwrite', 'Cancel', 'Cancel');
+    if strcmp(choice, 'Cancel')
+        disp('Operation cancelled.');
+        sam_skipped = 1;
+    elseif strcmp(choice, 'Overwrite')
+        disp(['Existing data for ', subj, ' will be overwritten.']);
+        % Find the row and overwrite it
+        rowIdx = find(strcmp(sam.subj, subj));
+        sam(rowIdx, :) = [];
+        sam_skipped = 0;
+    end
+else
+    sam_skipped = 0;
+end
 
+if sam_skipped ~= 1
+    % Create new row
+    newRow = cell(1, width(sam));
+    newRow{1} = subj;
 
+    % Input values for each variables
+    for i = 2:width(sam)
+        varName = sam.Properties.VariableNames{i};
 
-% Update Protocol
-protocol{subj_idx,1} = subj;
-protocol{subj_idx,2} =  datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss');
-protocol{subj_idx,3} = 'ALL OK';
+        while true
+            answer = inputdlg([varName ' (Angeben: Ganze Zahl)'], 'SAM', [1 70]);
+            if isempty(answer)
+                disp('Cancelled.');
+                return;
+            end
+            % If the answer is empty, keep prompting
+            if isempty(answer{1})
+                disp('This field cannot be left empty. Please enter a valid response.');
+            else
+                % Otherwise, store the answer and break out of the loop
+                newRow{i} = answer{1};
+                break;
+            end
+        end
+    end
+
+    % Add new row to the table
+    newRowTable = cell2table(newRow, 'VariableNames', sam.Properties.VariableNames);
+    for i = 1:width(sam)
+        if iscell(sam{:, i}) && ~iscell(newRowTable{:, i})
+            newRowTable{:, i} = {newRowTable{:, i}};
+        end
+    end
+    sam = [sam; newRowTable];
+end
 
 % Put the old data into an archive
 tid_psam_clean_up_folder_TD(OUTPATH)
 
 % Save new FAL data
 writetable(fal, fal_filename);
-disp([subj, ' has been added and saved.']);
+disp([subj, ' FAL has been added and saved.']);
+
+% Save new NASA-TLX data
+writetable(nasatlx, nasatlx_filename);
+disp([subj, ' NASA-TLX has been added and saved.']);
+
+% Save new SAM data
+writetable(sam, sam_filename);
+disp([subj, ' SAM has been added and saved.']);
+
 
