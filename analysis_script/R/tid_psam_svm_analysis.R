d9b18ebe4ce7e5e2d80d06d2ea43ff0b7dc60d9b
Tim Dreßler <tim.dressler@uni-oldenburg.de>
2025-06-02 23:26:12 +0200

    Major Updates

---
+++ analysis_script/R/tid_psam_svm_analysis.R
@@ -0,0 +1,117 @@
+# tid_psam_beh_analysis.R
+#
+# Performs analysis of behavioural data.
+#
+# Tim Dressler, 18.04.25
+
+# NOTES
+# Check ANOVA Type before running, for some cases I changed the type from III to I in order for it to run with the limited amount of pilot data
+
+#-------------------------------------Set up------------------------------------
+#load packages
+library(tidyr) 
+library(afex)
+library(emmeans)
+library(readxl)
+library(car) 
+library(corrplot) 
+library(dplyr) 
+library(rstudioapi)
+library(ez)
+library(ggplot2)
+library(ggstatsplot)
+library(DescTools)
+library(ggpubr)
+library(ggeffects)
+library(cowplot) 
+library(tidyverse)
+library(psych)
+library(RVAideMemoire)
+library(rstatix)
+
+rm(list=ls())
+
+options(scipen = 999)
+
+# Set costum colors
+colors <- list()
+colors$main_blue <- "#004F9F"
+colors$main_red <- "#D53D0E"
+colors$main_green <- "#00786B"
+colors$light_blue <- "#5BC5F2"
+colors$main_yellow <- "#FDC300"
+colors$UI <- "grey"
+
+# Set up paths
+SCRIPTPATH <- dirname(rstudioapi::getSourceEditorContext()$path)
+if (grepl("psam/analysis_script/R", SCRIPTPATH)) {
+  cat("Path OK\n") 
+} else {
+  stop("Path not OK")  
+}
+
+MAINPATH <- gsub("/analysis_script/R", "", SCRIPTPATH)
+INPATH <- file.path(MAINPATH, "data", "analysis_data", "svm_analysis")
+OUTPATH <- file.path(MAINPATH, "data", "analysis_data", "stats_svm_analysis")
+
+FUNPATH <- file.path(MAINPATH, "functions")
+source(file.path(FUNPATH, "tid_psam_check_folder_TD.R"))
+source(file.path(FUNPATH, "tid_psam_clean_up_folder_TD.R"))
+
+tid_psam_check_folder_TD(MAINPATH, INPATH, OUTPATH)
+tid_psam_clean_up_folder_TD(OUTPATH)
+
+setwd(OUTPATH)
+
+#-----------------------------------Load data-----------------------------------
+
+# Load data
+df_svm_wide <- read_excel(file.path(INPATH, "all_subj_accuracy_proportions.xlsx"))
+
+df_svm <- df_svm_wide %>% # Same df in long format 
+  pivot_longer(cols = starts_with("prop"), names_to = "feature_extraction_window", values_to = "percent_above_chance")
+df_svm$feature_extraction_window <- as.factor(df_svm$feature_extraction_window)
+
+#------------------------------------Analysis-----------------------------------
+
+# MAIN_SVM1
+# Paired T-Test (DV = Percentage of above chance level performing hyperparameter pairs, within = Temporal window (early, late))
+# Analysis MAIN_SVM1 concerns how the percentage of above chance level performing hyperparameter pairs is influenced by the time window used for extracting the features. 
+MAIN_SVM1 <- t.test(
+  df_svm_wide$prop_sig_early,
+  df_svm_wide$prop_sig_late,
+  paired = TRUE
+)
+MAIN_SVM1
+
+MAIN_SVM1_ES <- as.data.frame(df_svm) %>% 
+  cohens_d(percent_above_chance ~ feature_extraction_window, paired = TRUE)
+MAIN_SVM1_ES
+
+# Plot: Probe F0 by probe-type
+ggplot(df_svm, aes(x = feature_extraction_window, y = percent_above_chance, fill = feature_extraction_window)) +
+  geom_boxplot(show.legend = T) +  
+  theme_ggstatsplot() 
+
+# Descriptive statistics
+psych::describeBy(df_svm$percent_above_chance,
+                  group = df_svm$feature_extraction_window)
+
+# Assumptions 
+# Normal distibution
+df_svm %>%
+  ggplot(aes(x = percent_above_chance)) +
+  geom_histogram(bins = 50) +
+  facet_wrap(df_svm$feature_extraction_window) +
+  theme_ggstatsplot()
+
+byf.shapiro(percent_above_chance ~ feature_extraction_window, 
+            data = df_svm)
+
+#------------------------------------------------------------------------------#
+#
+#
+
+# Assumptions
+# - Normal distribution:  
+#------------------------------------------------------------------------------#
